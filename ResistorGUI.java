42c205776d37b00642758afa4004a97583d2d075
xNewz <pargornruasijan@gmail.com>
2023-11-03 15:33:23 +0700

    update: formated code

--- ResistorGUI.java
+++ ResistorGUI.java
@@ -1,62 +1,71 @@
-import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
+import javax.swing.*;
 
 public class ResistorGUI {
-    private Resistor resistor;
 
-    private JFrame frame;
-    private JButton generateButton;
-    private JLabel colorLabel;
-    private JLabel resistanceLabel;
-    private JLabel toleranceLabel;
+  private Resistor resistor;
 
-    public ResistorGUI() {
-        resistor = new Resistor();
-        frame = new JFrame("Resistor Color Code");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 200);
-        frame.setLayout(null);
+  private JFrame frame;
+  private JButton generateButton;
+  private JLabel colorLabel;
+  private JLabel resistanceLabel;
+  private JLabel toleranceLabel;
 
-        generateButton = new JButton("Generate Color Code");
-        generateButton.setBounds(150, 20, 200, 30);
-        frame.add(generateButton);
+  public ResistorGUI() {
+    resistor = new Resistor();
+    frame = new JFrame("Resistor Color Code");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400, 200);
+    frame.setLayout(null);
 
-        colorLabel = new JLabel("Resistor Colors:");
-        colorLabel.setBounds(20, 60, 300, 30);
-        frame.add(colorLabel);
+    generateButton = new JButton("Generate Color Code");
+    generateButton.setBounds(150, 20, 200, 30);
+    frame.add(generateButton);
 
-        resistanceLabel = new JLabel("Resistance:");
-        resistanceLabel.setBounds(20, 90, 200, 30);
-        frame.add(resistanceLabel);
+    colorLabel = new JLabel("Resistor Colors:");
+    colorLabel.setBounds(20, 60, 300, 30);
+    frame.add(colorLabel);
 
-        toleranceLabel = new JLabel("Tolerance:");
-        toleranceLabel.setBounds(20, 120, 200, 30);
-        frame.add(toleranceLabel);
+    resistanceLabel = new JLabel("Resistance:");
+    resistanceLabel.setBounds(20, 90, 200, 30);
+    frame.add(resistanceLabel);
 
-        generateButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String[] colors = resistor.randomColor();
-                double resistance = resistor.calculateResistance(colors);
-                String tolerance = resistor.calculateTolerance(colors);
+    toleranceLabel = new JLabel("Tolerance:");
+    toleranceLabel.setBounds(20, 120, 200, 30);
+    frame.add(toleranceLabel);
 
-                colorLabel.setText("Resistor Colors: " + Arrays.toString(colors));
-                resistanceLabel.setText("Resistance: " + resistor.formatResistance(resistance) + " ohms");
-                toleranceLabel.setText("Tolerance: " + resistor.formatTolerance(tolerance));
-            }
-        });
+    generateButton.addActionListener(
+      new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          String[] colors = resistor.randomColor();
+          double resistance = resistor.calculateResistance(colors);
+          String tolerance = resistor.calculateTolerance(colors);
 
-        frame.setVisible(true);
-    }
+          colorLabel.setText("Resistor Colors: " + Arrays.toString(colors));
+          resistanceLabel.setText(
+            "Resistance: " + resistor.formatResistance(resistance) + " ohms"
+          );
+          toleranceLabel.setText(
+            "Tolerance: " + resistor.formatTolerance(tolerance)
+          );
+        }
+      }
+    );
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new ResistorGUI();
-            }
-        });
-    }
+    frame.setVisible(true);
+  }
+
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(
+      new Runnable() {
+        @Override
+        public void run() {
+          new ResistorGUI();
+        }
+      }
+    );
+  }
 }
